8160e618a60bcddddf0c5de3c28b58c77ce7f798
martingrosek <martin.grosek@student.um.si>
2024-03-07 16:26:19 +0100

    Initial commit

---
+++ src/main/java/com/feri/bookmanagment/BookManagmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.feri.bookmanagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookManagmentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feri/bookmanagment/BookManagmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feri.bookmanagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
