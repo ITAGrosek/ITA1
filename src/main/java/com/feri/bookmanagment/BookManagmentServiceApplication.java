411f83629c58bf23f4ddbbb5f644bb09bc57798d
martingrosek <martin.grosek@student.um.si>
2024-03-09 10:06:38 +0100

    Nova baza

--- src/main/java/com/feri/bookmanagment/BookManagmentServiceApplication.java
+++ src/main/java/com/feri/bookmanagment/BookManagmentServiceApplication.java
@@ -1,5 +1,6 @@
 package com.feri.bookmanagment;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -9,6 +10,7 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 public class BookManagmentServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(BookManagmentServiceApplication.class, args);
 	}
 
