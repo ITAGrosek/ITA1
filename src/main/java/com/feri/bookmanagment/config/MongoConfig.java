90a9e3e2e0f680150db1fb736c184f72624c4753
martingrosek <martin.grosek@student.um.si>
2024-03-10 10:44:09 +0100

    Docker

--- src/main/java/com/feri/bookmanagment/config/MongoConfig.java
+++ src/main/java/com/feri/bookmanagment/config/MongoConfig.java
@@ -1,7 +1,7 @@
 package com.feri.bookmanagment.config;
 
 import com.mongodb.client.MongoClients;
-import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
@@ -9,15 +9,14 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 @Configuration
 public class MongoConfig {
 
-    private final Dotenv dotenv;
+    @Value("${MONGO_URI}")
+    private String mongoUri;
 
-    public MongoConfig() {
-        dotenv = Dotenv.load();
-    }
+    @Value("${DB_NAME}")
+    private String dbName;
 
     @Bean
     public MongoTemplate mongoTemplate() {
-        String uri = dotenv.get("MONGO_URI");
-        return new MongoTemplate(MongoClients.create(uri), dotenv.get("DB_NAME"));
+        return new MongoTemplate(MongoClients.create(mongoUri), dbName);
     }
 }
