7322c2a91d6e2005eec537bceb1b048d99083aad
martingrosek <martin.grosek@student.um.si>
2024-03-11 19:35:43 +0100

    env

--- src/main/java/com/feri/bookmanagment/config/MongoConfig.java
+++ src/main/java/com/feri/bookmanagment/config/MongoConfig.java
@@ -4,8 +4,28 @@ import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 
+
+@Configuration
+public class MongoConfig {
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
+    @Bean
+    public MongoTemplate mongoTemplate() {
+        // Uporabite SimpleMongoClientDatabaseFactory za ustvarjanje MongoDatabaseFactory
+        MongoDatabaseFactory factory = new SimpleMongoClientDatabaseFactory(mongoUri);
+        return new MongoTemplate(factory);
+    }
+}
+
+
+
+/*
 @Configuration
 public class MongoConfig {
 
@@ -20,3 +40,4 @@ public class MongoConfig {
         return new MongoTemplate(MongoClients.create(mongoUri), dbName);
     }
 }
+*/
